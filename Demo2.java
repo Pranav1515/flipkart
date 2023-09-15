9854908747d654bc29c2c686cc9f772d8dfe85da
Pranav1515 <pranav2suknale@rediffmail.com>
2023-09-15 13:28:26 +0530

    1 msg added

--- Demo2.java
+++ Demo2.java
@@ -12,6 +12,8 @@ public class Demo {
 		
 		driver =new ChromeDriver();
 		System.out.println("Hello Pranav");
+		System.out.println("Hi Pranav");
+
 		driver.get("https://nichethyself.com/tourism/home.html");
 		
 	}
